b718419da4b65764f2e6169ed2bbfc6daef1bb2c
Moritz Gorny <morit.gorny@fau.de>
2024-10-24 10:35:49 +0200

    Added stupid dummy tests for the satisfaction of having the github action sho a green check mark - it used to fail because there were no test files for b04 through b06.

---
+++ test/adap-b04/coordinates/Coordinate.test.ts
@@ -0,0 +1,8 @@
+import { it, expect, describe } from 'vitest'
+import { Coordinate } from "../../../src/adap-b04/coordinates/Coordinate";
+
+describe('Dummy Test to satisfy github workflow', () => {
+    it("still a dummy, don't trust it!", () => {
+        expect(true).toBe(true);
+    });
+});
