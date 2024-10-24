504ae83df353fe57716350014f3b514511b5e35a
Moritz Gorny <moritz.gorny@proton.me>
2024-10-24 11:37:25 +0200

    deleted dummy tests, because I added --paasWithNoTests to test command.

--- test/adap-b04/coordinates/Coordinate.test.ts
+++
@@ -1,8 +0,0 @@
-import { it, expect, describe } from 'vitest'
-import { Coordinate } from "../../../src/adap-b04/coordinates/Coordinate";
-
-describe('Dummy Test to satisfy github workflow', () => {
-    it("still a dummy, don't trust it!", () => {
-        expect(true).toBe(true);
-    });
-});
--- test/adap-b06/common/Cloneable.test.ts
+++
@@ -1,8 +0,0 @@
-import { it, expect, describe } from 'vitest'
-import { Cloneable } from "../../../src/adap-b05/common/Cloneable";
-
-describe('Dummy Test to satisfy github workflow', () => {
-    it("still a dummy, don't trust it!", () => {
-        expect(true).toBe(true);
-    });
-});
